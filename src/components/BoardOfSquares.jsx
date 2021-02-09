6ac3e800a982d42c34d300d8d7bb88e55099f0e3
ma-kun0122 <hirooka.masashi@gmail.com>
2021-02-09 22:22:07 +0900

    2/9

--- src/components/BoardOfSquares.jsx
+++ src/components/BoardOfSquares.jsx
@@ -1,32 +1,57 @@
 import React from 'react';
+import './BoardOfSquares.css';
+
 
 class BoardOfSquares extends React.Component {
+
     render() {
       return (
-        <div>
+      <div>
+        <h1>TIC TAC TOE</h1>
+        <div className="display">
+          <div className="turn">
+            <div className="turn-item circle active">○</div>
+            <div className="turn-item cross">×</div>
+          </div>
+          <div className="state">
+            <span className="state-message"></span>
+          </div>
+        </div>
+
+
          <table　className =　"table">
            <tbody>
                 <tr className = "row">
-                  <td className = "cell">"○"</td>
-                  <td className = "cell">"○"</td>
-                  <td className = "cell">"○"</td>
+                  <td className = "cell">○</td>
+                  <td className = "cell">○</td>
+                  <td className = "cell">○</td>
                 </tr>
 
                 <tr className = "row">
-                  <td className = "cell">"○"</td>
-                  <td className = "cell">"○"</td>
-                  <td className = "cell">"○"</td>
+                  <td className = "cell">○</td>
+                  <td className = "cell">○</td>
+                  <td className = "cell">○</td>
                 </tr>
                 
                 <tr className = "row">
-                  <td className = "cell">"○"</td>
-                  <td className = "cell">"○"</td>
-                  <td className = "cell">"○"</td>
+                  <td className = "cell">○</td>
+                  <td className = "cell">○</td>
+                  <td className = "cell">○</td>
                 </tr>
 
               </tbody>
             </table>
-        </div>
+
+            <div class="l-footer footer">
+              <div class="state-message js-state-message">starting...</div>
+              <div>
+                <a class="button js-restart ">RESTART</a>
+              </div>
+            </div>
+        
+      </div>
+
+
       )
     }
   }
